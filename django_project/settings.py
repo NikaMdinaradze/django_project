741faba8105aed06ed56e41f6540b3a846723b41
NikaMdinaradze <nikolozi.mdinaradze.1@bt.edu.geu>
2023-12-03 23:17:41 +0400

    crispy forms doesn't work

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
 
     # my_apps
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig'
+    
+    # third-party
+    'crispy-forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
