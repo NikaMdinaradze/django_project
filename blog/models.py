9a33ba06cb26818859e83ea749297191c06c6a11
NikaMdinaradze <mdinaradzenika04@gmail.com>
2023-12-06 16:47:24 +0400

    8 video

--- blog/models.py
+++ blog/models.py
@@ -11,10 +11,6 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    class Meta:
-        verbose_name = 'პოსტი'
-        verbose_name_plural = 'პოსტები'
-
     def __str__(self):
         return self.title
 
