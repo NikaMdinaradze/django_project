156091b797cb7255907ff0c90c88c43ab0af4751
NikaMdinaradze <mdinaradzenika04@gmail.com>
2023-12-07 18:42:04 +0400

    video 10

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -23,4 +23,21 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.POST:
+        u_forms = UserUpdateForm(request.POST,instance=request.user)
+        p_forms = ProfileUpdateForm(request.POST,
+                                    request.FILES,
+                                    instance=request.user.profile)
+        if p_forms.is_valid() and u_forms.is_valid():
+            p_forms.save()
+            u_forms.save()
+            messages.success(request, 'your profile has been updated')
+            redirect('profile')
+    else:
+        u_forms = UserUpdateForm(instance=request.user)
+        p_forms = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_forms': u_forms,
+        'p_forms': p_forms
+    }
+    return render(request, 'users/profile.html', context)
